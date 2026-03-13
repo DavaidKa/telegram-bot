b68f347f6c73832743a839dc623a0d36263ce51d
DavaidKa <datu121010@gmail.com>
2026-03-13 03:16:48 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,24 @@
 # Handlers package
+# Импорт всех обработчиков
+
+from . import start
+from . import content
+from . import games
+from . import premium
+from . import referral
+from . import admin
+from . import homework
+from . import exam_helper
+from . import photo_solver
+
+__all__ = [
+    'start',
+    'content',
+    'games',
+    'premium',
+    'referral',
+    'admin',
+    'homework',
+    'exam_helper',
+    'photo_solver',
+]
